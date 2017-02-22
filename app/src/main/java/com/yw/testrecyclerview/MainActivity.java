e067805e4368fca817d815e9b2a8b2341c7c8e8c
yingwei0831 <yingwei0831@163.com>
2017-02-22 13:58:46 +0800

    为主页MainActivity RecyclerView 添加下拉刷新、上拉加载

--- app/src/main/java/com/yw/testrecyclerview/MainActivity.java
+++ app/src/main/java/com/yw/testrecyclerview/MainActivity.java
@@ -2,10 +2,13 @@ package com.yw.testrecyclerview;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.Handler;
+import android.os.Message;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
@@ -14,6 +17,7 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
+import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -26,12 +30,27 @@ import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RecyclerView recyclerView;
+    private SwipeRefreshLayout mSwipeRefreshLayout;
+    private RecyclerView mRecyclerView;
 
     private List<String> mDates;
 
     private RecyclerViewSimpleAdapter mAdapter;
 
+    private LinearLayoutManager mLayoutManager;
+    private int lastVisibleItem;
+
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what){
+                case 0:
+                    mSwipeRefreshLayout.setRefreshing(false);
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,15 +63,15 @@ public class MainActivity extends AppCompatActivity {
         initDatas();
         initViews();
         mAdapter = new RecyclerViewSimpleAdapter(getApplicationContext(), mDates);
-        recyclerView.setAdapter(mAdapter);
+        mRecyclerView.setAdapter(mAdapter);
 
         //设置RecyclerView的布局管理
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false);
-        recyclerView.setLayoutManager(linearLayoutManager);
+        mLayoutManager = new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false);
+        mRecyclerView.setLayoutManager(mLayoutManager);
         //设置RecyclerView的item间分隔线
-        recyclerView.addItemDecoration(new DividerItemDecoration(getApplicationContext(), DividerItemDecoration.VERTICAL_LIST, getResources().getDrawable(R.drawable.divider_hori)));
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(getApplicationContext(), DividerItemDecoration.VERTICAL_LIST, getResources().getDrawable(R.drawable.divider_hori)));
         //设置Item增加、移除动画
-        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
 
         mAdapter.setOnItemClickListener(new RecyclerViewSimpleAdapter.onItemClickListener() {
 
@@ -76,8 +95,57 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initViews() {
-        recyclerView = (RecyclerView)findViewById(R.id.id_recycler_view);
 
+        mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh_layout);
+        mSwipeRefreshLayout.setColorSchemeColors(Color.GREEN, Color.BLUE, Color.RED); //最多可以设置4种颜色
+
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                // 此处在现实项目中，请换成网络请求数据代码，sendRequest .....
+                handler.sendEmptyMessageDelayed(0, 3000);
+            }
+        });
+//        这句话是为了，第一次进入页面的时候显示加载进度条
+        mSwipeRefreshLayout.setProgressViewOffset(false, 0,
+                (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 24, getResources().getDisplayMetrics())); //调整进度条距离屏幕顶部的距离
+        mRecyclerView = (RecyclerView)findViewById(R.id.id_recycler_view);
+        mRecyclerView.setHasFixedSize(true); //高度固定不变，提高性能
+        mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            /**
+             * Callback method to be invoked when RecyclerView's scroll state changes.
+             *
+             * @param recyclerView The RecyclerView whose scroll state has changed.
+             * @param newState     The updated scroll state. One of {@link #SCROLL_STATE_IDLE},
+             *                     {@link #SCROLL_STATE_DRAGGING} or {@link #SCROLL_STATE_SETTLING}.
+             */
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                if (newState == RecyclerView.SCROLL_STATE_IDLE && lastVisibleItem + 1 == mAdapter.getItemCount()){
+                    mSwipeRefreshLayout.setRefreshing(true);
+                    // 此处在现实项目中，请换成网络请求数据代码，sendRequest .....
+                    handler.sendEmptyMessageDelayed(0, 3000);
+                }
+            }
+
+            /**
+             * Callback method to be invoked when the RecyclerView has been scrolled. This will be
+             * called after the scroll has completed.
+             * <p/>
+             * This callback will also be called if visible item range changes after a layout
+             * calculation. In that case, dx and dy will be 0.
+             *
+             * @param recyclerView The RecyclerView which scrolled.
+             * @param dx           The amount of horizontal scroll.
+             * @param dy           The amount of vertical scroll.
+             */
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                lastVisibleItem = mLayoutManager.findLastVisibleItemPosition();
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_floating_action_button);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -108,13 +176,13 @@ public class MainActivity extends AppCompatActivity {
             case R.id.action_setting:
                 break;
             case R.id.action_gridview:
-                recyclerView.setLayoutManager(new GridLayoutManager(getApplicationContext(), 3));
+                mRecyclerView.setLayoutManager(new GridLayoutManager(getApplicationContext(), 3));
                 break;
             case R.id.action_listview:
-                recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+                mRecyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
                 break;
             case R.id.action_hor_girdview:
-                recyclerView.setLayoutManager(new StaggeredGridLayoutManager(6, StaggeredGridLayoutManager.HORIZONTAL));
+                mRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(6, StaggeredGridLayoutManager.HORIZONTAL));
                 break;
             case R.id.action_stagger:
                 Intent intent = new Intent(getApplicationContext(), StaggeredGridLayoutActivity.class);
