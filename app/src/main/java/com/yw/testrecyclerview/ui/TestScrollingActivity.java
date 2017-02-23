041f6da989e501923fb902c7056c88a5c4828e65
yingwei0831 <yingwei0831@163.com>
2017-02-23 15:03:37 +0800

    为ScrollingActivity添加系统自带的返回箭头，并能够点击、响应

--- app/src/main/java/com/yw/testrecyclerview/ui/TestScrollingActivity.java
+++ app/src/main/java/com/yw/testrecyclerview/ui/TestScrollingActivity.java
@@ -3,13 +3,19 @@ package com.yw.testrecyclerview.ui;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.yw.testrecyclerview.R;
 
 public class TestScrollingActivity extends AppCompatActivity {
+
+    private static final String TAG = "TestScrollingActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,14 +23,32 @@ public class TestScrollingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_test_scrolling);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true); //1.显示toolbar返回箭头
+        ab.setHomeButtonEnabled(true); //2.让toolbar返回箭头可以点击
 
         final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
-//                Snackbar.make(fab, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Log.e(TAG, "------onOptionsItemSelected------");
+        switch (item.getItemId()){
+            case android.R.id.home: //3.系统添加toobar返回箭头的id事件
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
